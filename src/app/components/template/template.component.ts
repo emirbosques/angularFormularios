ac069878faf66bd19804d913ded560889365e4cb
Emir Bosques <emirbosques@gmail.com>
2019-03-23 17:06:45 +0100

    Validacion de Varios imputs

--- src/app/components/template/template.component.ts
+++ src/app/components/template/template.component.ts
@@ -11,8 +11,28 @@ export class TemplateComponent implements OnInit {
   public usuario = {
     name: null,
     apellido: null,
-    email: null
+    email: null,
+    pais: '',
+    sexo: '',
+    conditions:false
   };
+
+  paises = [
+    {cod: 'ESP', name: 'ESPAÑA'},
+    {cod: 'USA', name: 'ESTADOS UNIDOS'},
+    {cod: 'NOR', name: 'NORUEGA'},
+    {cod: 'AUT', name: 'AUSTRIA'},
+    {cod: 'VEN', name: 'VENEZUELA'},
+    {cod: 'MEX', name: 'MEXICO'}
+  ];
+
+  sexo = [
+    {cod: 'M', name: 'Mujer'},
+    {cod: 'H', name: 'Hombre'},
+    {cod: 'T', name: 'Transexual'},
+    {cod: 'I', name: 'Indefinido'}
+  ];
+
 
   constructor() { }
 
